c8d6779305167960b7157b1a7d229439d22e7a03
Marcos Garcia de Lima <marckfree@gmail.com>
2017-08-28 18:59:12 -0300

    Start stylus compilation

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	// entry: "./src/.js",
+	//
+	// output: {
+	// 	path: __dirname,
+	// 	filename: "bundle.js"
+	// },
+	//
+	// module: {
+	// 	loaders: [
+	// 		{ test: /\.css$/, loader: "style!css" }
+	// 	]
+	// }
+};
