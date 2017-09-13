ceb04264e856b75afac3a93bc4d530a79c20d56b
Marcos Garcia de Lima <marckfree@gmail.com>
2017-09-13 14:40:22 -0300

    Add Vue.js

--- src/js/app/modules/foobar.js
+++ src/js/app/modules/foobar.js
@@ -1,3 +1,11 @@
-(() => {
-	console.log('Hello world!');
-})();
+const Vue = require('../../node_modules/vue/dist/vue.common.js');
+
+const App = new Vue({
+	delimiters: ['${', '}'],
+
+	el: '#app',
+
+	data:  {
+		message: 'world!'
+	}
+});
